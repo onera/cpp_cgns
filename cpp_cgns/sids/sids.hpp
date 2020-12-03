335f14aa988132e3dc33420dbc92466bc0870722
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2020-12-03 23:25:17 +0100

    Minor

---
+++ cpp_cgns/sids/sids.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "cpp_cgns/sids/Building_Block_Structure_Definitions.hpp"
+#include "cpp_cgns/sids/Hierarchical_Structures.hpp"
+#include "cpp_cgns/sids/Grid_Coordinates_Elements_and_Flow_Solution.hpp"
+#include "cpp_cgns/sids/Multizone_Interface_Connectivity.hpp"
+
+#include "cpp_cgns/sids/utils.hpp"
---
+++ cpp_cgns/test/tree_manip.test.cpp
@@ -0,0 +1,9 @@
+#include "std_e/unit_test/doctest.hpp"
+#include "cpp_cgns/tree_manip.hpp"
+
+
+using namespace cgns;
+
+
+TEST_CASE("") {
+}
