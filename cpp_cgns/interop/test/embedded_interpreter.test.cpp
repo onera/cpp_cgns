467fe89565a3e987b5948400ed44193c79f1bcfc
Bérenger Berthoul <Berenger.Berthoul@onera.fr>
2022-12-21 15:16:41 +0100

    Minor: change C++ syntax to please Clang

--- cpp_cgns/interop/test/embedded_interpreter.test.cpp
+++ cpp_cgns/interop/test/embedded_interpreter.test.cpp
@@ -2,5 +2,5 @@
 
 // needed to run some tests in C++ runtime (where no python interpreter has been started yet)
 namespace {
-  const pybind11::scoped_interpreter python_interpreter = {};
+  const auto _ = pybind11::scoped_interpreter{};
 }
